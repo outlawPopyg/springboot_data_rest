bf41e18a32e78fc6558ac8d079a0f59aab5ea526
Kalim <kalim.ahmetchin2003@gmail.com>
2022-07-26 14:17:05 +0300

    fefef

---
+++ src/main/java/com/example/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.spring_data_rest.dao;
+
+import com.example.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    List<Employee> findAllByName(String name);
+}
